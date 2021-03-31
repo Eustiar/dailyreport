11a868d3400345b970d128524d0549b6ab7172d5
Eustiar <eustiar@qq.com>
2021-03-31 14:18:46 +0800

    coding problem

--- dailyreport_xg.py
+++ dailyreport_xg.py
@@ -7,6 +7,7 @@ import requests
 import datetime
 import schedule
 import re
+import yagmail
 
 sess = requests.session()
 login_url = 'http://xgsm.hitsz.edu.cn/zhxy-xgzs/xg_mobile/xsHome'
@@ -18,6 +19,8 @@ with open('userdata', 'r') as f:
     data = f.readlines()
     username = data[0].strip()
     password = data[1].strip()
+    email_username = data[2].strip()
+    email_password = data[3].strip()
 
 def login():
     res = sess.get(login_url, headers=ua)
@@ -27,17 +30,23 @@ def login():
                   'vc_username': None, 'vc_password': None}
     res = sess.post(certification_url, headers=ua, data=login_data)
     if '头部' in res.text:
-        save_log('login success!')
+        save_log('Login success!')
         return True
-    save_log('login failed!')
+    save_log('Login failed!')
     return False
 
 def save_log(data_log):
     with open(log_path, 'a+') as f:
         f.write('\n' + str(datetime.datetime.now()) + '\n\n' + data_log + '\n')
 
+def send_email(text, host='smtp.126.com', target_email = 'eustiar@qq.com'):
+    yag = yagmail.SMTP(user=email_username, password=email_password, host=host)
+    contents = [text]
+    yag.send(target_email, 'subject', contents)
+
 def work_once():
     if not login():
+        send_email('Login failed')
         print('Login failed')
         exit()
     res = sess.post(add_url, headers=ua)
@@ -46,15 +55,17 @@ def work_once():
     edit_url = 'http://xgsm.hitsz.edu.cn/zhxy-xgzs/xg_mobile/xs/saveYqxx'
     formdata = {'info': '{"model":{"id":"' + module +
                         '","dqszd":"01","hwgj":"","hwcs":"",'
-                        '"hwxxdz":"","dqszdsheng":"440000","dqszdshi":"440300","dqszdqu":"440305","gnxxdz":"哈工大",'
+                        '"hwxxdz":"","dqszdsheng":"440000","dqszdshi":"440300","dqszdqu":"440305","gnxxdz":"HIT",'
                         '"dqztm":"01","dqztbz":"","brfsgktt":"0","brzgtw":"35.5","brsfjy":"","brjyyymc":"",'
                         '"brzdjlm":"","brzdjlbz":"","qtbgsx":"","sffwwhhb":"0","sftjwhjhb":"0","tcyhbwhrysfjc":"0",'
                         '"sftzrychbwhhl":"0","sfjdwhhbry":"0","tcjtfs":"","tchbcc":"","tccx":"","tczwh":"","tcjcms":"",'
                         '"gpsxx":"","sfjcqthbwhry":"0","sfjcqthbwhrybz":"","tcjtfsbz":""}}'}
     res = sess.post(edit_url, headers=ua, data=formdata)
     if res.json()['isSuccess']:
+        send_email('Add success')
         save_log('Add success')
     else:
+        send_email('Add failed')
         save_log('Add failed')
 
 
